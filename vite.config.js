d325434e9bff699348af9aa1265ea1b207d410b6
noble.kclee <noble.kclee@gmail.com>
2026-04-06 10:09:56 +0900

    fix: _redirects 원복 - assets 404 규칙 삭제

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   build: {
     outDir: 'dist',
     minify: 'terser',
     cssMinify: true,
-    assetsDir: 'assets',
   },
 })
